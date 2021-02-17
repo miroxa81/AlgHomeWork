f34caf11c5ecd902adfa878cd9d7034f104e0639
miroxa81 <miroxa81@hotmail.com>
2021-02-17 19:54:50 +0200

    Finish project

--- Lesson1/Lesson1/Task1/Program.cs
+++ Lesson1/Lesson1/Task1/Program.cs
@@ -6,7 +6,65 @@ namespace Task1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n;
+
+			WriteColor($"Пожалуйста введите число:", ConsoleColor.Gray);
+			do
+				if (!int.TryParse(Console.ReadLine(), out n))
+				{
+					WriteColor($"Пожалуйста введите число:", ConsoleColor.DarkRed);
+				}
+				else break;
+			while (true);
+
+
+			if (PrimeNumber(n))
+			{
+
+				WriteColor($"Число {n} простое", ConsoleColor.DarkGreen);
+			}
+			else
+			{
+				WriteColor($"Число {n} не являеться простым", ConsoleColor.DarkYellow);
+			}
+
+			Console.ReadKey();
+		}
+
+
+		static bool PrimeNumber(int number)
+		{
+			int d = 0;
+			int i = 2;
+	
+
+			while (i < number)
+			{
+				if (number % i == 0) d++;
+				i++;
+			}
+
+			if (d == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		static void WriteColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.Write(txt);
+			Console.ResetColor();
+		}
+		static void WriteLineColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.WriteLine(txt);
+			Console.ResetColor();
 		}
 	}
 }
