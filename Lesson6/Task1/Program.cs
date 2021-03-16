63a7147842a45ea5bc69ec67d48e673571790b02
Miroslav <miroxa81@hotmail.com>
2021-03-16 15:50:26 +0200

    !

--- Lesson6/Task1/Program.cs
+++ Lesson6/Task1/Program.cs
@@ -5,14 +5,15 @@ namespace Task1
 {
 	class Program
 	{
+	
 		static void Main(string[] args)
 		{
 
 			Queue<int> nodelist = new Queue<int>();
+			string[] head = new string[9] {"", "A", "B", "C", "D", "E", "F", "G", "H", };
+			int[,] matrix = new int[8, 8];
 
-			int[,] matrix = new int[10, 10];
-
-			for (int i = 0; i < matrix.GetLength(0); i++)
+				for (int i = 0; i < matrix.GetLength(0); i++)
 				for (int j = 0; j < matrix.GetLength(1); j++)
 				{
 					if (i != j)
@@ -20,17 +21,27 @@ namespace Task1
 						matrix[i, j] = new Random().Next(0, 20);
 					}
 				}
-			for (int i = 0; i < matrix.GetLength(0); i++)
+			for (int i = 0; i <= matrix.GetLength(0); i++)
 			{
-				for (int j = 0; j < matrix.GetLength(1); j++)
+				for (int j = 0; j <= matrix.GetLength(1); j++)
 				{
+					if (i == 0)
+					{
+						WriteColor($"{head[j],3}",ConsoleColor.Green);
+					}
+					else
+					if (j == 0)
+					{
+						WriteColor($"{head[i],3}", ConsoleColor.Green);
+					}
+					else
 					if (i == j)
 					{
-						WriteColor($"{matrix[i, j],3}", ConsoleColor.Yellow);
+						WriteColor($"{matrix[i-1, j-1],3}", ConsoleColor.Yellow);
 					}
 					else
 					{
-						WriteColor($"{matrix[i, j],3}", ConsoleColor.Cyan);
+						WriteColor($"{matrix[i-1, j-1],3}", ConsoleColor.Cyan);
 					}
 				}
 				Console.WriteLine();
