8402c7c5279544e1a08df3e6a6710616eace099c
Miroslav <miroxa81@hotmail.com>
2021-03-16 15:05:43 +0200

    !

--- Lesson6/Task1/Program.cs
+++ Lesson6/Task1/Program.cs
@@ -7,18 +7,53 @@ namespace Task1
 	{
 		static void Main(string[] args)
 		{
-			const int n= 20; // кол-во вершин графа
-			List<Node> Graph = new List<Node>();
 
+			Queue<int> nodelist = new Queue<int>();
 
+			int[,] matrix = new int[10, 10];
 
-			for (int i = 0; i <n; i++)
+			for (int i = 0; i < matrix.GetLength(0); i++)
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					if (i != j)
+					{
+						matrix[i, j] = new Random().Next(0, 20);
+					}
+				}
+			for (int i = 0; i < matrix.GetLength(0); i++)
 			{
-				Node newNode = new Node();
-				
-				newNode.Name = new RandomString().Next(5);
-				newNode.Edges.
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					if (i == j)
+					{
+						WriteColor($"{matrix[i, j],3}", ConsoleColor.Yellow);
+					}
+					else
+					{
+						WriteColor($"{matrix[i, j],3}", ConsoleColor.Cyan);
+					}
+				}
+				Console.WriteLine();
 			}
+		}
+
+
+
+
+
+
+
+		static void WriteColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.Write(txt);
+			Console.ResetColor();
+		}
+		static void WriteLineColor(string txt, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.WriteLine(txt);
+			Console.ResetColor();
 		}
 	}
 }
