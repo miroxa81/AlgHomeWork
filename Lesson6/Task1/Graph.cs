a3ed0e157ac5454f2a03e8784adadc1c2acae0c8
Miroslav <miroxa81@hotmail.com>
2021-03-16 21:39:14 +0200

    !

--- Lesson6/Task1/Graph.cs
+++ Lesson6/Task1/Graph.cs
@@ -4,15 +4,16 @@ using System.Text;
 
 namespace Task1
 {
-		public class Node //Вершина
-		{
-			public string Name { get; set; }
-			public List<Edge> Edges { get; set; } //исходящие связи
-		}
-		public class Edge //Ребро
-		{
-			public int Weight { get; set; } //вес связи
-			public Node Node { get; set; } // узел, на который связь ссылается
-		}
-	
+	public class Node //Вершина
+	{
+		public string Name { get; set; }
+		public List<Edge> Edges { get; set; } //исходящие связи
+	}
+	public class Edge //Ребро
+	{
+		public int Weight { get; set; } //вес связи
+		public Node Node { get; set; } // узел, на который связь ссылается
+	}
+
+
 }
--- Lesson6/Task1/Program.cs
+++ Lesson6/Task1/Program.cs
@@ -5,29 +5,32 @@ namespace Task1
 {
 	class Program
 	{
-	
+		const int n = 8;
 		static void Main(string[] args)
 		{
+			Queue<Node> nodelist = new Queue<Node>();
+			
+			int[] peaks = new int[n];
+			int[,] matrix = RandomMatrix(n);
 
-			Queue<int> nodelist = new Queue<int>();
-			string[] head = new string[9] {"", "A", "B", "C", "D", "E", "F", "G", "H", };
-			int[,] matrix = new int[8, 8];
+					
+			OutMatrix(matrix);
+			Node[] FilledGraph = FillGraph(matrix);
+		}
 
-				for (int i = 0; i < matrix.GetLength(0); i++)
-				for (int j = 0; j < matrix.GetLength(1); j++)
-				{
-					if (i != j)
-					{
-						matrix[i, j] = new Random().Next(0, 20);
-					}
-				}
+
+
+		static void OutMatrix(int[,] matrix)
+		{
+			string[] head = new string[9] { "", "A", "B", "C", "D", "E", "F", "G", "H" };
+
 			for (int i = 0; i <= matrix.GetLength(0); i++)
 			{
 				for (int j = 0; j <= matrix.GetLength(1); j++)
 				{
 					if (i == 0)
 					{
-						WriteColor($"{head[j],3}",ConsoleColor.Green);
+						WriteColor($"{head[j],3}", ConsoleColor.Green);
 					}
 					else
 					if (j == 0)
@@ -37,21 +40,54 @@ namespace Task1
 					else
 					if (i == j)
 					{
-						WriteColor($"{matrix[i-1, j-1],3}", ConsoleColor.Yellow);
+						WriteColor($"{matrix[i - 1, j - 1],3}", ConsoleColor.Yellow);
 					}
 					else
 					{
-						WriteColor($"{matrix[i-1, j-1],3}", ConsoleColor.Cyan);
+						WriteColor($"{matrix[i - 1, j - 1],3}", ConsoleColor.Cyan);
 					}
 				}
 				Console.WriteLine();
 			}
+
 		}
 
 
+		/*public List<int> WalkDFS()//Lesson 5 
+		{
+			List<int> DFSlist = new List<int>();
 
 
+			var TreeStack = new Stack<TreeNode>();
+			TreeStack.Push(root);
 
+			while (TreeStack.Count > 0)
+			{
+				var element = TreeStack.Pop();
+				DFSlist.Add(element.Data);
+
+				if (element.RightChild != null) TreeStack.Push(element.RightChild);
+				if (element.LeftChild != null) TreeStack.Push(element.LeftChild);
+			}
+
+			return DFSlist;
+		}
+		public List<int> WalkBFS()//Lesson 5 
+		{
+			List<int> BFSlist = new List<int>();
+			var TreeQueue = new Queue<TreeNode>();
+			TreeQueue.Enqueue(root);
+
+			while (TreeQueue.Count > 0)
+			{
+				var element = TreeQueue.Dequeue();
+				BFSlist.Add(element.Data);
+				if (element.LeftChild != null) TreeQueue.Enqueue(element.LeftChild);
+				if (element.RightChild != null) TreeQueue.Enqueue(element.RightChild);
+
+			}
+			return BFSlist;
+		}*/
 
 
 		static void WriteColor(string txt, ConsoleColor color)
